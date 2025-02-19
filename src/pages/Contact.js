1019ff2ac968b75237382e7c672ce21d1a41b29a
deshanrewis <deshanr.ewis@gmail.com>
2025-02-19 18:32:32 +0530

    contact - map updated

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -50,7 +50,7 @@ function Contact() {
         <p>Visit our wood yard to explore premium wood chips, sawdust, and firewood.</p>
         <iframe
           title="Google Map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509588!2d144.95373531531713!3d-37.816279742021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0:0x0!2zMjMuNzgxOTExLCBMYXRpdHVkZQ!5e0!3m2!1sen!2sus!4v1700000000000!5m2!1sen!2sus"
+          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d35558.652392194424!2d80.30245506133316!3d7.021684593524251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zN8KwMDEnNDIuMCJOIDgwwrAxOCc0NS45IkU!5e0!3m2!1sen!2slk!4v1739969835384!5m2!1sen!2slk"
           allowFullScreen
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
